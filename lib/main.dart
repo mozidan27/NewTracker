45c17c09663fb2408b3a207b0bd4ea00bfa11856
iamzidan <zidann256@gmail.com>
2024-11-17 09:09:34 +0200

    welcome page created

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:new_tracker/welcome_page.dart';
 
 void main() {
   runApp(const NewTracker());
@@ -9,6 +10,9 @@ class NewTracker extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: WelcomePage(),
+    );
   }
 }
