1d79478f03256f2a414edfd5e0c192051aee1ae3
iamzidan <zidann256@gmail.com>
2024-11-17 08:27:38 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/new_tracker/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.new_tracker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const NewTracker());
+}
+
+class NewTracker extends StatelessWidget {
+  const NewTracker({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
